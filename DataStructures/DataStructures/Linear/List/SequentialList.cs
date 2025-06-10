67a8d14169aec9c59436dc366fdd284d5774887b
Michealmiker <michealmiker@gmail.com>
2025-06-10 19:43:38 +0800

    向表尾插入新元素

--- DataStructures/DataStructures/Linear/List/SequentialList.cs
+++ DataStructures/DataStructures/Linear/List/SequentialList.cs
@@ -8,16 +8,62 @@
 public class SequentialList<T>(int capacity)
 {
     /// <summary>
+    /// 表长度
+    /// </summary>
+    public int Count { get; private set; } = 0;
+    
+    /// <summary>
     /// 表容量
     /// </summary>
     public int Capacity { get; private set; } = capacity;
+
+    private T[] _elements = new T[capacity];
 
     /// <summary>
     /// 默认容量
     /// </summary>
     private const int DefaultCapacity = 4;
 
+    /// <summary>
+    /// 增量倍数
+    /// </summary>
+    private const int IncrementCapacity = 2;
+
     public SequentialList() : this(DefaultCapacity)
     {
+    }
+
+    /// <summary>
+    /// 将新元素插入表尾
+    /// </summary>
+    /// <param name="item"></param>
+    public void Add(T item)
+    {
+        // 1. 检查表容量是否足够
+        CheckCapacity();
+
+        // 2. 将新元素插入表尾
+        _elements[Count] = item;
+
+        // 3. 表长度加1
+        Count++;
+    }
+
+    public override string ToString() => string.Join(", ", _elements.Take(Count));
+
+    /// <summary>
+    /// 检查表容量
+    /// </summary>
+    private void CheckCapacity()
+    {
+        if (Count < Capacity)
+        {
+            return;
+        }
+        
+        // 如果表已满，则扩充表容量
+        Capacity *= IncrementCapacity;
+        
+        Array.Resize(ref _elements, Capacity);
     }
 }
--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -1,2 +1,11 @@
 ﻿var seqList = new SequentialList<int>();
 
+for (var i = 0; i < 10; i++)
+{
+    seqList.Add(i + 1);
+}
+
+Console.WriteLine("表信息：");
+Console.WriteLine($"表容量：{seqList.Capacity}");
+Console.WriteLine($"表长度：{seqList.Count}");
+Console.WriteLine($"表内容:\n{seqList}");
